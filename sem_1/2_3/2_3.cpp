817c22bdbe921122037fbb0a5669008d350b49f3
kostyazakharov589 <rompompom3497@gmail.com>
2025-11-25 16:17:39 +0500

    Update 2_3.cpp

--- sem_1/2_3/2_3.cpp
+++ sem_1/2_3/2_3.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n, s = 0;
+    std::cin >> n;
+    if (n > 0){
+        for (int i = 1;i<=n;i++){
+            s = i*(i+1);
+        }
+        std::cout<<s<<std::endl;
+    }
+    else{
+        std::cout<<"ввести больше 0"<<std::endl;
+    }
+    
+return 0;
+}
 
