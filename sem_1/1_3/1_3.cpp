de7cd70a37e4a52af0ecf10868a225aed3fa5f77
kostyazakharov589 <rompompom3497@gmail.com>
2025-11-25 15:23:26 +0500

    Create 1_3.cpp

---
+++ sem_1/1_3/1_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a;
+	cin >> a;
+	if (a < 5){
+	    cout<<a*3<<endl;
+	}
+	if (a >= 5 and a <= 7){
+	    cout<<a+3<<endl;
+	}
+	else{
+	    cout<<a/10<<endl;
+	}
+return 0;
+}
