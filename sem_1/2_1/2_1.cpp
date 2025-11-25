c33c0b7109cdfb74f1c9e290ee7d7598a93aee07
kostyazakharov589 <rompompom3497@gmail.com>
2025-11-25 15:42:28 +0500

    Update 2_1.cpp

--- sem_1/2_1/2_1.cpp
+++ sem_1/2_1/2_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n, s = 0;
+    std::cin >> n;
+    if (n >= 0){
+        for (int i = 1;i<=n;i++){
+            s+=i;
+        }
+        std::cout<<s<<std::endl;
+    }
+    else{
+        std::cout<<"ввести больше 0"<<std::endl;
+    }
+    
+return 0;
+}
 
