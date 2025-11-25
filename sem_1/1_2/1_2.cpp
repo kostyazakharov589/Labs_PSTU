9bafbb2e4af6fea5c87a51864ccc4d36e8c9f7fe
kostyazakharov589 <rompompom3497@gmail.com>
2025-11-25 14:59:39 +0500

    Create 1_2.cpp

---
+++ sem_1/1_2/1_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b;
+	cin >> a >> b;
+	if (b == 0){
+	    cout<<"не делить на 0"<<endl;
+	}
+	else{
+	    cout<<a/b<<endl;
+	}
+return 0;
+}
