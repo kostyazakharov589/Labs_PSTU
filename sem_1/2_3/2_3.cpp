1005e9d249b060c77f4eb035f088133ef641d69d
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-02 16:08:56 +0500

    Update 2_3.cpp

--- sem_1/2_3/2_3.cpp
+++ sem_1/2_3/2_3.cpp
@@ -1,17 +1,19 @@
-#include <iostream> 
-int main(){
-    int n;
-    int a, b = 0;
+#include <iostream>
+
+int main() {
+	int n;
+    int tmp, sum = 0;
     std::cin >> n;
     for (int i = 1; i <= n; i++)
-        {
-            a = 1;
-            for (int j = i; j <= 2*i; j++)
-            {
-                a *= j;
-            }
-        b += a;
-        }
-    std::cout << a << std::endl;
+    {
+    tmp = 1;
+        for (int j = i; j <= 2*i; j++)
+    {
+    tmp *= j;
+    }
+    sum += tmp;
+}
+    std::cout << sum << std::endl;
+
 }
 
