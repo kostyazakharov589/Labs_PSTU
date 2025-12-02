fdcef853c55b791e428714afe58a0f53ac9875e7
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-02 14:34:00 +0500

    Update 2_4.cpp

--- sem_1/2_4/2_4.cpp
+++ sem_1/2_4/2_4.cpp
@@ -1,1 +1,26 @@
-
+#include <iostream> 
+int main(){
+    int n;
+    std::cin >> n;
+    int k = n / 2;
+    int s = 1;
+    if (n>3,n%2!=0){
+        for (int i = 1; i <= (n+1)/2; i++)
+        {
+        for (int j = 0; j < k; j++)
+        {
+            std::cout << " ";
+        }
+        k--;
+        for (int t = 0; t < s; t++)
+        {
+            std::cout << "*";
+        }
+        s += 2;
+        std::cout << std::endl;
+        }
+    }
+    if( n <= 3, n%2 == 0){
+        std::cout << "больше 3 или нечётное число" << std::endl;
+    }
+}
