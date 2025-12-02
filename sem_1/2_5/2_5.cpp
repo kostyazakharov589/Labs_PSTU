fc509b7b988cb5848efe98605e0b26bc86bb3c4e
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-02 15:05:56 +0500

    Update 2_5.cpp

--- sem_1/2_5/2_5.cpp
+++ sem_1/2_5/2_5.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream> 
+int main(){
+    int n;
+    std::cin >>n;
+    if(n>2){
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j<n-1;j++){
+                std::cout << " ";
+            }
+            for(int k = 0; k<=i;k++){
+                std::cout << "*";
+            }
+        std::cout << std::endl;
+        }
+    }
+}
