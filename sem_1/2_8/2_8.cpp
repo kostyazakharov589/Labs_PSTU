c528a929df6119658d1d3af3d74c6ab2a3fc14d8
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-02 16:19:43 +0500

    Update 2_8.cpp

--- sem_1/2_8/2_8.cpp
+++ sem_1/2_8/2_8.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cin >> n;
+	if(n > 2){
+	    for(int i = 0; i < n; i++)
+	    {
+	    for(int j = 0; j <= i; j++)
+	    {
+	        std::cout << "* ";
+	    }
+	        
+	        std::cout << std::endl;
+    	}
+	    }
+	else{
+	    std::cout<< "больше 2" << std::endl;
+	}
+}
 
