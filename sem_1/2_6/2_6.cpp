db72ce30e945b105845f235c3c26d20c45a85dbe
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-02 15:11:23 +0500

    Update 2_6.cpp

--- sem_1/2_6/2_6.cpp
+++ sem_1/2_6/2_6.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream> 
+int main(){
+    int n;
+    std::cin >>n;
+    if(n>2){
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j<=i;j++){
+                std::cout << " ";
+            }
+            for(int k = 0; k<n-i;k++){
+                std::cout << "*";
+            }
+        std::cout << std::endl;
+        }
+    }
+}
