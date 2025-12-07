76348442eaf1436ea64790dd7b49d9937053600d
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:45:35 +0500

    Update 2_9.cpp

--- sem_1/2_9/2_9.cpp
+++ sem_1/2_9/2_9.cpp
@@ -1,4 +1,40 @@
 #include <iostream> 
 int main(){
+int n, d;
+cin >> n;
+d = n/2;
+
+for (int i = 0; i < d; i++)
+{
+ cout << " ";
+}
+for (int i = 0; i < n; i++)
+{
+ cout << "* ";
+}
+cout << endl;
+
+for (int i = 0; i < d-1; i++)
+{
+ for (int j = 1; j < d-i; j++)
+ {
+ cout << " ";
+ }
+ cout << "* ";
+ for (int j = 0; j < n-2; j++)
+ {
+ cout << " ";
+ }
+ cout << "*";
+ for (int j = 1; j < i; j++)
+ {
+ cout << " ";
+ }
+ cout << "*" << endl;
+}
+
+for (int i = 0; i < n; i++)
+{
+ cout << "
 return 0;
 }
