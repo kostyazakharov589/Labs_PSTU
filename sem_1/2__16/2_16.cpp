49f0b8e935a311b8530820be9911a5063caf67b8
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:55:37 +0500

    Update 2_16.cpp

--- sem_1/2__16/2_16.cpp
+++ sem_1/2__16/2_16.cpp
@@ -1,4 +1,13 @@
 #include <iostream> 
 int main(){
+int n;
+int sum = 0;
+cin >> n;
+for (int i = 1; i <= n; i++)
+{
+ if (i % 2 == 0) { sum += i; }
+ else { sum -= i; }
+}
+cout << sum << endl;
 return 0;
 }
