9e87d34c2eab210302c8308e3355de072d887bd8
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:55:02 +0500

    Update 2_15.cpp

--- sem_1/2__15/2_15.cpp
+++ sem_1/2__15/2_15.cpp
@@ -1,4 +1,18 @@
 #include <iostream> 
 int main(){
+int n, s, k;
+bool flag = false;
+cin >> n >> s;
+while (n > 0 && !flag)
+{
+ k = n % 10;
+ if (k == s)
+ {
+ flag = true;
+ cout << "Цифра найдена" << endl;
+ }
+ else { n /= 10; }
+}
+if (!flag) { cout << "Цифра не найдена" << endl; }
 return 0;
 }
