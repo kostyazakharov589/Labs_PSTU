8edf00b6737d2b8010ee87cdca7712cd61efd787
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:46:47 +0500

    Update 2_10.cpp

--- sem_1/2__10/2_10.cpp
+++ sem_1/2__10/2_10.cpp
@@ -1,4 +1,16 @@
 #include <iostream> 
 int main(){
+int n, tmp, max;
+cout << "длина последовательности:" << endl;
+cin >> n;
+cout << "Первое число:" << endl;
+cin >> max;
+for (int i = 2; i <= n; i++)
+{
+ cout << "Следующее число:" << endl;
+ cin >> tmp;
+ if (tmp > max) { max = tmp; }
+}
+cout << "Макс. эл.: " << max << endl;
 return 0;
 }
