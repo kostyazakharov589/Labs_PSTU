94396e85f882d1d56db05ecc508087d839d023ab
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:53:26 +0500

    Update 2__13.cpp

--- sem_1/2__13/2__13.cpp
+++ sem_1/2__13/2__13.cpp
@@ -1,4 +1,21 @@
 #include <iostream> 
+#include <cmath>
 int main(){
+
+float a, b, c, d, x1, x2;
+cin >> a >> b >> c;
+d = pow(b, 2) - 4*a*c; // b*b - 4*a*c
+if (d > 0)
+{
+ x1 = (-b + sqrt(d)) / (2*a);
+ x2 = (-b - sqrt(d)) / (2*a);
+ cout << "x1=" << x1 << "; x2=" << x2 << endl;
+}
+else if (d == 0)
+{
+ x1 = (-b) / (2*a)
+ cout << "x1=x2=" << x1 << endl;
+}
+else { cout << "Корней нет" << endl; }
 return 0;
 }
