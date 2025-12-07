5681cd52a8234715b615fd1e604721d1177117ae
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-07 17:52:15 +0500

    Update 2__12.cpp

--- sem_1/2__12/2__12.cpp
+++ sem_1/2__12/2__12.cpp
@@ -1,4 +1,20 @@
 #include <iostream> 
 int main(){
+int n, tmp, max, min;
+cout << "Введите длину последовательности:" << endl;
+cin >> n;
+cout << "Введите первое число:" << endl;
+cin >> tmp;
+max = tmp;
+min = tmp;
+for (int i = 2; i <= n; i++)
+{
+ cout << "Введите следующее число:" << endl;
+ cin >> tmp;
+ if (tmp > max) { max = tmp; }
+ else if (tmp < min) { min = tmp; }
+}
+cout << "Сумма максимального и минимального элементов: " << (max+min) <<
+endl;
 return 0;
 }
