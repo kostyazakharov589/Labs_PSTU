d3538dc3a589a0c3ac310f29055c8237ae4f9717
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-08 22:12:23 +0500

    Update 2_16.cpp

--- sem_1/2__16/2_16.cpp
+++ sem_1/2__16/2_16.cpp
@@ -6,11 +6,16 @@ int s = 0;
 std::cin >> n;
 for (int i = 1; i <= n; i++)
 {
- if (i % 2 == 0) { s += i; }
- else { s -= i; }
+ if (i % 2 == 0) {
+  s += i; 
+ }
+ else { 
+  s -= i; 
+ }
 }
 std::cout << sum << std::endl;
 
 return 0;
 }
+
 
