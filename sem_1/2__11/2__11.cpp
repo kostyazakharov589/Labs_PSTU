a908f511311969a21f00fba1a071a70ee431fc4c
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-08 22:06:54 +0500

    Update 2__11.cpp

--- sem_1/2__11/2__11.cpp
+++ sem_1/2__11/2__11.cpp
@@ -4,11 +4,11 @@ int main(){
 int n, tmp;
 bool plus1 = false; 
 bool minus1 = false; 
-cout << "Введите длину последовательности:" << endl;
-cin >> n;
+std::cout << "Введите длину последовательности:" << std::endl;
+std::cin >> n;
 for (int i = 1; i <= n; i++)
 {
- cin >> tmp;
+ std::cin >> tmp;
  if (!plus1 && !minus1)
  {
  if (tmp > 0) { plus1 = true; }
@@ -16,9 +16,10 @@ for (int i = 1; i <= n; i++)
  }
 }
 if (plus1) { cout << "Сначала ввели положительное число"; }
-else if (minus1) { cout << "Сначала ввели отрицательное число"; }
-else { cout << "Все элементы были нулевыми"; }
+else if (minus1) { std::cout << "Сначала ввели отрицательное число"; }
+else { std::cout << "Все элементы были нулевыми"; }
 
-cout << endl;
+std::cout << std::endl;
 return 0;
 }
+
