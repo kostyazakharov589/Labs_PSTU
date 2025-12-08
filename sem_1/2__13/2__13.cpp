573a81e2221ae75c3366c0fdba5ba12ba43de308
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-08 22:04:34 +0500

    Update 2__13.cpp

--- sem_1/2__13/2__13.cpp
+++ sem_1/2__13/2__13.cpp
@@ -4,20 +4,21 @@ using namespace std;
 int main(){
 
 float a, b, c, d, x1, x2;
-cin >> a >> b >> c;
+std::cin >> a >> b >> c;
 d = pow(b, 2) - 4*a*c; // b*b - 4*a*c
 if (d > 0)
 {
  x1 = (-b + sqrt(d)) / (2*a);
  x2 = (-b - sqrt(d)) / (2*a);
- cout << "x1=" << x1 << "; x2=" << x2 << endl;
+ std::cout << "x1=" << x1 << "; x2=" << x2 << std::endl;
 }
 else if (d == 0)
 {
  x1 = (-b) / (2*a)
- cout << "x1=x2=" << x1 << endl;
+ std::cout << "x1 = x2=" << x1 << std::endl;
 }
-else { cout << "Корней нет" << endl; }
+else { std::cout << "Корней нет" << std::endl; }
 
 return 0;
 }
+
