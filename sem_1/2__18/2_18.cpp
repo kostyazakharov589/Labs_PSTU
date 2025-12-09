1e68e149588ea44c71246532a2302930ca9ea964
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-09 13:53:53 +0500

    Update 2_18.cpp

--- sem_1/2__18/2_18.cpp
+++ sem_1/2__18/2_18.cpp
@@ -1,5 +1,18 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main(){
-return 0;
+
+int main() {
+	int d, n, c = 0;
+	std::cout << "Ввести число"<< std::endl;
+	std::cin >> n;
+	while( n != 0){
+	    d = n % 10;   
+        c = c * 10 + d;   
+        n = n / 10;
+	}
+	std::cout << c << std::endl;
+	
+	
+	return 0;
+
 }
