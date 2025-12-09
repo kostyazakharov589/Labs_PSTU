e87aebcb4250ac401e976d55d242ec487eda3937
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-09 14:33:12 +0500

    Update 2_19.cpp

--- sem_1/2__19/2_19.cpp
+++ sem_1/2__19/2_19.cpp
@@ -1,5 +1,21 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main(){
-return 0;
+
+int main() {
+    int n, s = 0;
+    std::cout << "Введите кол-во слагаемых " << std::endl;
+    std::cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        int r = i % 3;  
+        if (r == 1 || r == 2){
+            s += i;       
+        }
+        else{
+            s -= i;
+        }
+    }
+
+    std::cout << s << std::endl;
+    return 0;
 }
