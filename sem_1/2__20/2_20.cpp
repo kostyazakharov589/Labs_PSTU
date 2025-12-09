35e2cc16823ba3c3f86ec36fd580f119e2693e1e
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-09 15:25:39 +0500

    Update 2_20.cpp

--- sem_1/2__20/2_20.cpp
+++ sem_1/2__20/2_20.cpp
@@ -1,5 +1,25 @@
-#include <iostream> 
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-return 0;
+
+int main() {
+    float n,s,a;
+    int i = 1;
+    bool flag = false;
+    std::cin>> n >> s;
+    while (i<= n || !flag){
+        a = sin(n+ i/n);
+        a = round(a*10)/10;
+        std::cout<< n+i/n << "|" << a << std::endl;
+        if( a== s) {
+            flag = true;
+        }
+        else{ i++;}}
+    if (flag){
+        std::cout << i << std::endl;
+    }
+    else{
+        std::cout<< "эл. не найден"<<std::endl;
+    }
+    return 0;
 }
