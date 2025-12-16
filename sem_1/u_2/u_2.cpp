c4bffe7771e5e56cd4fc9cbe428508e05e6d41fb
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 09:33:16 +0500

    Update u_2.cpp

--- sem_1/u_2/u_2.cpp
+++ sem_1/u_2/u_2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+int main() {
+	int a = 10;
+	int b = 20;
+
+	std::cout << "a - " << a << " b - " << b <<  std::endl;
+
+	int *ua = &a;
+	int *ub = &b;
+
+	int c = *ub;
+
+	*ub = *ua;
+	*ua = c;
+
+	std::cout << "a - " << a << " b - " << b << std::endl;
+}
