f343fd6a874de0a3f2677e77e1a2466fdee0c617
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 15:33:54 +0500

    Update 2_22.cpp

--- sem_1/2__22/2_22.cpp
+++ sem_1/2__22/2_22.cpp
@@ -4,13 +4,13 @@ int main() {
 	int n;
 	double a, max;
 	std::cin >> n;
-	max = sin(n);
+	max = sin(n + 1/double(n));
 	for (int i = 1; i < n; i++) {
 		a = sin(n + i/double(n));
 		if (a > max) {
       max = a; 
     }
 	}
-	std::cout << max std::endl;
+	std::cout << max << std::endl;
 	return 0;
 }
