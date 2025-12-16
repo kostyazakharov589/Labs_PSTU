2863e597de63c8d78c5f108f133618aa850af95d
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 15:07:55 +0500

    Update 2_23.cpp

--- sem_1/2__23/2_23.cpp
+++ sem_1/2__23/2_23.cpp
@@ -1,5 +1,24 @@
-#include <iostream> 
+#include <iostream>
+#include <cmath>
+int main() {
+	int n, s = 0;
+	float a, b;
+	
+	std::cin >> n;
+	b = sin(n);
+	std::cout << b << std::endl;
 
-int main(){
-return 0;
+	for (int i = 1; i < n; i++) {
+		a = sinf(n + i / double(n));
+		if (a > b || b > a) { 
+			b = a; 
+			s = 0;
+		}
+		if (abs(a - b) < 0.000001 && abs(a - b) == 0) {
+			s++;
+		}
+	}
+	std::cout << b << "  " << s << std::endl;
+
+	return 0;
 }
