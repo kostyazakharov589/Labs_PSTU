2d8bf2b04f84cbc42ba81b226bbdd2035b6a1a2e
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 09:37:49 +0500

    Update u_3.cpp

--- sem_1/u_3/u_3.cpp
+++ sem_1/u_3/u_3.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+int main() {
+	float a = 2.1;
+	float b = 10.4;
+	std::cout << "a " << a << "  b " << b << std::endl;
+
+	float *ua = &a;
+	float *ub = &b;
+	float s = *ua + *ub;
+	std::cout << s << std::endl;
+
+	return 0;
+}
+
 
