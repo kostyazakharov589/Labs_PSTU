0f77e65c2ee98d7f16e1af275c207a5c1480fff8
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 09:55:13 +0500

    Update u_4.cpp

--- sem_1/u_4/u_4.cpp
+++ sem_1/u_4/u_4.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+int main() {
+	int b;
+	float max, min, a;
 
+	std::cin >> b;
+	std::cin >> max;
+	float *umax = &max;
+	float *umin = &min;
+	*umin = *umax;
+
+	for (int i = 1; i < b; i++) {
+		std::cin >> a;
+		if (a > *umax) 
+		{
+			*umax = a;
+		}
+		if (a < *umin) 
+		{
+			*umin = a;
+		}
+	}
+    std::cout << "max " << *umax << "  min " << *umin << std::endl;
+}
