2d9be3aa4646db27b67fc40fa9872f1876568c84
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 15:45:55 +0500

    Update 2_22.cpp

--- sem_1/2__22/2_22.cpp
+++ sem_1/2__22/2_22.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <cmath>
 int main() {
-	int n;
-	double a, max;
+	int num = 1;
+	float a, max, n;
 	std::cin >> n;
-	max = sin(n + 1/double(n));
-	for (int i = 1; i < n; i++) {
-		a = sin(n + i/double(n));
-		if (a > max) {
-      max = a; 
-    }
-	}
-	std::cout << max << std::endl;
+	max = sin(n + 1/n);
+	int i = 2;
+    while (i <= n)
+    {
+        a = sin(n + i/n);
+        if (a > max)
+        {
+            max = a;
+            num = i;
+        }
+        i++;
+}
+	std::cout << max << "  " << num << std::endl;
 	return 0;
 }
