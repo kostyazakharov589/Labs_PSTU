c32d87345fcb2a164b41285797a6b968f72fa0fa
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 10:14:34 +0500

    Update 2_21.cpp

--- sem_1/2__21/2_21.cpp
+++ sem_1/2__21/2_21.cpp
@@ -1,5 +1,23 @@
-#include <iostream> 
-
-int main(){
-return 0;
+#include <iostream>
+int main() {
+	int n, b;
+	bool f = 1;
+	std::cin >> b;
+	
+	while (true) {
+		std::cin >> n;
+		if (b > n){ 
+		    f = 0; 
+		}
+	    if (n == 0){
+		    break;
+		}
+	}
+	if (f == 1 && n == 0) {
+		std::cout << "возрастающая";
+	}
+	if (f == 0) {
+		std::cout << "не возрастающая";
+	}
+	return 0;
 }
