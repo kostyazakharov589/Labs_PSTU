7a777254194fd5fa5d69c1033e8db047aaed23a6
kostyazakharov589 <rompompom3497@gmail.com>
2025-12-16 17:34:18 +0500

    Update eq_Newton.cpp

--- sem_1/eq_Newton/eq_Newton.cpp
+++ sem_1/eq_Newton/eq_Newton.cpp
@@ -1,1 +1,27 @@
+//Вариант 12    0.1*a^2 - x*ln(x)  0,2x−lnx−1
+
+#include <iostream>
+#include <cmath>
+int main()
+{
+    float x, xPrev;
+    float t = 0.1; 
+    float t2 = 0.2;
+    
+    x = 2; // x0 = b;
+    xPrev = 0; // иницилизируем переменную x_{n-1} меньшим значением
+    double eps = 0.000001;
+
+     while(abs(x - xPrev) > eps)
+    {
+    xPrev = x;
+    // теперь пишем формулу, где x_{i-1} - f(x_{i-1} / f'(x_{i-1}))
+
+    x = xPrev - (((t*pow(x, 2)) - (x*log(x))) / (t2*x - (log(x)) - 1));
+ }
+ std::cout << "Корень: " << x << std::endl;
+ return 0;
+}
+
+
 
