38b2d570d12171d52bb00ea5ce290d950cbaf6d5
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-09 15:31:16 +0500

    Update 3_1.cpp

--- sem_2/3_1/3_1.cpp
+++ sem_2/3_1/3_1.cpp
@@ -1,1 +1,24 @@
+include <iostream>
+using namespace std;
+
+
+int main() {
+    const int n = 6;
+    int tmp;
+    int arr[n] = { 0, 2, 1, 3, 5, 9 };
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n / 2; i++) {
+        te\mp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = te\mp;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
 
