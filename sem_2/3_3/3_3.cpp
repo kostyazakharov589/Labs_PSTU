1ea2b6a7033ce36cbcdfe96eef2e4ba2ee0e6e2c
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-09 16:00:45 +0500

    Update 3_3.cpp

--- sem_2/3_3/3_3.cpp
+++ sem_2/3_3/3_3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+
+
+int main() {
+    const int n = 6;
+    int c = 0;
+    int max;
+    int arr[n] = { 0, 2, 1, 3, 5, 9 };
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    max = arr[0];
+    for (int i = 0; i < n; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+            c = 1;
+        }
+        else if (arr[i] = max) {
+            c += 1;
+        }
+    }
+        cout << max << " " << c << endl;
+    return 0;
+}
