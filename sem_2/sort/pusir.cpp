f2a8ed2d788a436d4103fb64e3e9faaf2fb4628e
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-13 20:03:57 +0500

    Update pusir.cpp

--- sem_2/sort/pusir.cpp
+++ sem_2/sort/pusir.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 6;
+	int tmp; 
+
+	int arr[n] = { 3, 8, 1, 17 , 5, 1 };
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
 
