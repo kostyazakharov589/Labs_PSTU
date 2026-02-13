9e9cc97fe2417055e9b3db7a0c016b6c56d35f54
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-13 20:10:13 +0500

    Update vstav.cpp

--- sem_2/sort/vstav.cpp
+++ sem_2/sort/vstav.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ const int n = 7;
+ int k; 
+int arr[n] = { 5, 2, 4, 6, 0, 1, 3};
+
+ for (int i = 0; i < n; i++)
+ {
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+
+ for (int i = 1; i < n-1; i++)
+ {
+ k = arr[i];
+ int j = i-1;
+ while (j >= 0 && arr[j] > k)
+ {
+ arr[j+1] = arr[j];
+ j--;
+ }
+ arr[j+1] = k;
+ }
+
+ for (int i = 0; i < n; i++)
+ {
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+ return 0;
+}
 
