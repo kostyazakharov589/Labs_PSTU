061b6e93a56c14725fda7d0d67d20cd339d9ed23
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-13 20:09:12 +0500

    Update vybor.cpp

--- sem_2/sort/vybor.cpp
+++ sem_2/sort/vybor.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ const int n = 7;
+ int tmp, min_index;
+
+ int arr[n] = { 5, 2, 4, 6, 0, 1, 3};
+
+ for (int i = 0; i < n; i++)
+ {
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+
+ for (int i = 0; i < n-1; i++)
+ {
+ min_index = i;
+ for (int j = i+1; j < n; j++)
+ {
+ if (arr[j] < arr[min_index])
+ { min_index = j; }
+ }
+ tmp = arr[min_index];
+ arr[min_index] = arr[i];
+ arr[i] = tmp;
+ }
+
+ for (int i = 0; i < n; i++)
+ {
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+ return 0;
+}
 
