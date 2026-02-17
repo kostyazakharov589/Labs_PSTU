5812bfd212416750642263f2c8d34a5602eb57a0
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-17 16:44:04 +0500

    Update and rename fibinachi.cpp to fibonachi.cpp

--- sem_2/fibinachi/fibonachi.cpp
+++ sem_2/fibinachi/fibonachi.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int FIBONACCI(int n) {
+int fibonachi(int n) {
 	if (n <= 1) return 0;
 	else if (n == 2) return 1;
-	return FIBONACCI(n - 2) + FIBONACCI(n - 1);
+	return fibonachi(n - 2) + fibonachi(n - 1);
 }
 
 int main() {
 	for (size_t i = 1; i < 20; i++) {
-		cout << FIBONACCI(i) << " ";
+		cout << fibonachi(i) << " ";
 	}
 }
