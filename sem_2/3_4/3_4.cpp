65e8e838086972efbf89a8e815067596cb8824d1
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-17 19:57:50 +0500

    Update 3_4.cpp

--- sem_2/3_4/3_4.cpp
+++ sem_2/3_4/3_4.cpp
@@ -2,26 +2,27 @@
 using namespace std;
 int main()
 {
-   const int n = 6;
+	const int n = 6;
+
+	int arr[n] = { 1, 2, 3, 4, 5, 6 };
+
+	bool sorted = true;
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < arr[i - 1])
+		{
+			sorted = false;
+			break;
+		}
+	}
+	cout << endl;
+
+	if (sorted)
+	{
+		cout << "отсортирован" << max << endl;
+	}
+	else { cout << "Массив не отсорт " << endl; }
 
 
-   int arr[n] = { 1, 2, 3, 4, 5, 6 };
-
- bool sorted = true;
- for (int i = 1; i < n; i++)
- {
-   if (arr[i] < arr[i-1])
- {
- sorted = false;
- break; // раннее прерывание
- }
- }
- cout << endl;
-
- if (sorted)
- { cout << "Массив отсортирован" << max << endl; }
- else { cout << "Массив не отсорт " << endl; }
-
-
- return 0;
+	return 0;
 }
