c0261b5a63ff0edadbb380b63034973eddd8018f
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-17 15:16:14 +0500

    Update nod.cpp

--- sem_2/nod/nod.cpp
+++ sem_2/nod/nod.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int nod(int n, int m) {
 	if (n == m) {
-    return n;
-  }
-	if (n > m) {
-    return nod(n - m, m);
-  }
-	return nod(n, m - n);
+		return n;
+	}
+	else if (n > m) {
+		return nod(n - m, m);
+	}
+	else {
+		return nod(n, m - n);
+	}
 }
 
 int main() {
-	cout << nod(17, 5) 	<< endl;
+	int n, m;
+	cin >> n;
+	cin >> m;
+	cout << nod(n, m) << endl;
 }
