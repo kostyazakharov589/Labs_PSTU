7875282b9a0fc59bc48484eddb64ffb0270188c2
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-17 19:23:16 +0500

    Create 3__12.cpp

---
+++ sem_2/3__12/3__12.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 5;
+	int arr[n][n] = {
+	{1, 1, 1, 1, 1},
+	{2, 2, 2, 2, 2},
+	{3, 3, 3, 3, 3},
+	{4, 4, 4, 4, 4},
+	{5, 5, 5, 5, 5} };
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i][n - 1 - i] = 0;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
