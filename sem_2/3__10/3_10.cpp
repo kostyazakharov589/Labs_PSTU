4a0171432956dd7442e9db2a76506077a460a6f0
kostyazakharov589 <rompompom3497@gmail.com>
2026-02-17 19:19:14 +0500

    Create 3_10.cpp

---
+++ sem_2/3__10/3_10.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 5;
+	int arr[n][n] = {
+	{1, 1, 1, 1, 1},
+	{2, 2, 2, 2, 2},
+	{3, 3, 3, 3, 3},
+	{4, 4, 4, 4, 4},
+	{5, 5, 5, 5, 5}
+	};
+	int tmp;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	// транспонируем матрицу
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			tmp = arr[i][j];
+			arr[i][j] = arr[j][i];
+			arr[j][i] = tmp;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
