258e4c518777acdf5387bd27ea910c211450c674
kostyazakharov589 <rompompom3497@gmail.com>
2026-03-04 23:43:50 +0500

    Update lab_6.cpp

--- sem_2/labs/lab_6/lab_6.cpp
+++ sem_2/labs/lab_6/lab_6.cpp
@@ -1,1 +1,38 @@
-6
+//Вариант 12 Преобразовать строку таким образом, чтобы все слова в ней были напечатаны наоборот
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system("cls");
+
+    char s[255];
+    int start = 0, c;
+    char t;
+
+    cout << "Введите строку и добавьте точку в конце " << endl;
+    cin.getline(s, 255);
+
+    int l = strlen(s);
+    for (int i = 0; i <= l; i++){
+        if (s[i] == ' ' || s[i] == '.' || s[i] == '\0'){
+            c = i - 1;
+            while (start < c)
+            {
+                t = s[start];
+                s[start] = s[c];
+                s[c] = t;
+
+                start++;
+                c--;
+            }
+            start = i + 1;
+        }
+    }
+
+    cout << "Результат: " << s << endl;
+    return 0;
+}
